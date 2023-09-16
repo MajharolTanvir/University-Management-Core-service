331089a9f0d5f07c929f0f86e910bd61177a0976
MajharolTanvir <majharul5456@gmail.com>
2023-09-16 11:40:06 +0600

    Academic semester zod validation functionality updated successfully

--- src/app/modules/academicSemester/academicSemester.constant.ts
+++ src/app/modules/academicSemester/academicSemester.constant.ts
@@ -12,7 +12,6 @@ export const AcademicSemesterSearchableFields = [
   'endMonth',
 ];
 
-
 export const academicSemesterTitleCodeMapper: {
   [key: string]: string;
 } = {
@@ -21,5 +20,21 @@ export const academicSemesterTitleCodeMapper: {
   Fall: '03',
 };
 
+export const academicSemesterTitle: string[] = ['Autumn', 'Summer', 'Fall'];
+export const academicSemesterCodes: string[] = ['01', '02', '03'];
+export const academicSemesterMonths: string[] = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
 
-export const EVENT_ACADEMIC_SEMESTER_CREATED = 'academic-semester-created'
+export const EVENT_ACADEMIC_SEMESTER_CREATED = 'academic-semester-created';
